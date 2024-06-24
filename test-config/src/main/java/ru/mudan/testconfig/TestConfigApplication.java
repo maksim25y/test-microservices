5a0b203454ae7808f5ba2d196b992f943b3d2a22
mudan <myudanov223@gmail.com>
2024-06-24 22:39:18 +0300

    added gateway,config

---
+++ test-config/src/main/java/ru/mudan/testconfig/TestConfigApplication.java
@@ -0,0 +1,15 @@
+package ru.mudan.testconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TestConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestConfigApplication.class, args);
+    }
+
+}
---
+++ test-config/src/test/java/ru/mudan/testconfig/TestConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mudan.testconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
